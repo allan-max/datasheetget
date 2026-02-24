f0d4535e2ba0826ec49452bf643ca576a6721917
allan-max <allansimaogames@gmail.com>
2026-02-24 08:36:04 -0300

    teste

--- scrapers/amazon.py
+++ scrapers/amazon.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
+#teste
 from .base import BaseScraper
 
 class AmazonScraper(BaseScraper):
@@ -65,7 +66,7 @@ class AmazonScraper(BaseScraper):
                     v = row.find("td")
                     if k and v:
                         specs[k.text.strip()] = v.text.strip()
-            # comentario para teste de git
+            
             # --- FILTRA AS SPECS ---
             specs = self.filtrar_specs(specs)
             if not specs: specs = {"Info": "Verificar descrição completa"}
